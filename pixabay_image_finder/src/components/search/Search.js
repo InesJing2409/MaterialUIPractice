159b9cd91c3cbb4d0432cc4f27ab2b9b83b5471a
unknown <inesjing0@gmail.com>
2019-08-15 17:07:45 +0800

    Resolve issues

--- pixabay_image_finder/src/components/search/Search.js
+++ pixabay_image_finder/src/components/search/Search.js
@@ -3,57 +3,79 @@ import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
 import axios from "axios";
+import ImageResults from "../image-results/ImageResults";
 
 const ranges = [
   {
-    value: 5,
+    value: "5",
     label: "5"
   },
   {
-    value: 10,
+    value: "10",
     label: "10"
   },
   {
-    value: 15,
+    value: "15",
     label: "15"
   },
   {
-    value: 30,
+    value: "30",
     label: "30"
   },
   {
-    value: 50,
+    value: "50",
     label: "50"
   }
 ];
 
 class Search extends Component {
   state = {
-    searchText: "",
-    amount: 15,
-    apiUrl: "https://pixabay.com.api",
+    searchText: "", // key phrase to search for
+    amount: 15, // number of images to return
+    apiUrl: "https://pixabay.com/api",
     apiKey: "13274676-1a7b8c9974a7eae6a936cbc3d",
-    images: []
+    images: [] // array to store all the images
+  };
+
+  callApi = () => {
+    axios
+      .get(
+        `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
+          this.state.searchText
+        }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
+      )
+      // after the promise is returned. res stands for response
+      .then(res => this.setState({ images: res.data.hits }))
+      .catch(err => console.log(err));
   };
 
   // e stands for event
   onTextChange = e => {
+    // value is the text inside the seatchText bar
+    const val = e.target.value;
     // in this case the target.name is searchText
-    this.setState({ [e.target.name]: e.target.value }, () => {
-      axios
-        .get(
-          `${this.state.apiUrl}/?key=${this.state.apiKey}&q=${
-            this.state.searchText
-          }&image_type=photo&per_page=${this.state.amount}&safesearch=true`
-        )
-        // after the promise is returned, res stands for response
-        .then(res => this.setState({ images: res.data.hits }))
-        .catch(err => console.log(err));
+    this.setState({ [e.target.name]: val }, () => {
+      if (val === "") {
+        console.log("val is null");
+        this.setState({ images: [] });
+      } else {
+        this.callApi();
+      }
+    });
+  };
+
+  // to change the number of images being displayed
+  onAmountChange = (e, index, value) => {
+    this.setState({ amount: value }, () => {
+      if (this.state.searchText === "") {
+        this.setState({ images: [] });
+      } else {
+        this.callApi();
+      }
     });
   };
 
   render() {
-    console.log(this.state.images);
     return (
       <div>
         <TextField
@@ -71,11 +93,18 @@ class Search extends Component {
           onChange={this.onAmountChange}
         >
           {ranges.map(option => (
-            <MenuItem key={option.value} value={option.value}>
-              {option.label}
-            </MenuItem>
+            <MenuItem
+              key={option.value}
+              primaryText={option.label}
+              value={option.value}
+            />
           ))}
         </SelectField>
+        <br />
+        {/* Display the images if there are any */}
+        {this.state.images.length > 0 ? (
+          <ImageResults images={this.state.images} />
+        ) : null}
       </div>
     );
   }
